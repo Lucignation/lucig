71500445e66f3a18f47dbaab87d778d6e2809561
Gerald Olumide <30971557+Lucignation@users.noreply.github.com>
2020-04-10 09:12:47 +0100

    all built files uploaded

---
+++ precache-manifest.dc9681f54b8b164c663564b30573d3fe.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80841f9172b1c8de3b82735d8a688fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f7572fd0ff42a41a561",
+    "url": "./static/css/2.b0443ea5.chunk.css"
+  },
+  {
+    "revision": "7b4f30de2e8106df488f",
+    "url": "./static/css/main.69550c7e.chunk.css"
+  },
+  {
+    "revision": "7f7572fd0ff42a41a561",
+    "url": "./static/js/2.f85f6472.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.f85f6472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4f30de2e8106df488f",
+    "url": "./static/js/main.2629da0f.chunk.js"
+  },
+  {
+    "revision": "065a90033d83ff76c2d4",
+    "url": "./static/js/runtime-main.bbd8f27a.js"
+  },
+  {
+    "revision": "95939c131ef6ae2f946cb42c214c8962",
+    "url": "./static/media/AgroAfrica.95939c13.png"
+  },
+  {
+    "revision": "2c60421aee1b06aab2ebb2180eee0a34",
+    "url": "./static/media/Java.2c60421a.png"
+  },
+  {
+    "revision": "c235d95abdb18366a848da2361329098",
+    "url": "./static/media/LUCIG3.c235d95a.png"
+  },
+  {
+    "revision": "b9de06ea0fd19f8ecd374571cf3952f3",
+    "url": "./static/media/ReactJS.b9de06ea.png"
+  },
+  {
+    "revision": "5d91115782278f1da74741b3ba837a00",
+    "url": "./static/media/apase.5d911157.png"
+  },
+  {
+    "revision": "2f997a87781fd6e40fb6c58320887a72",
+    "url": "./static/media/developer.2f997a87.jpg"
+  },
+  {
+    "revision": "c718e69bcab6eeb98ed7ad0b568b8cdc",
+    "url": "./static/media/developers.c718e69b.svg"
+  },
+  {
+    "revision": "bf17b0eac8314e6c25907f06ff1b3f26",
+    "url": "./static/media/footer.bf17b0ea.svg"
+  },
+  {
+    "revision": "da06fe2122eefdd8a506fe08e19aaf8b",
+    "url": "./static/media/frontend.da06fe21.jpg"
+  },
+  {
+    "revision": "6e044e86eb4e6b238c19a1f0da6b47cd",
+    "url": "./static/media/game.6e044e86.png"
+  },
+  {
+    "revision": "07d5212d6ae4a3d522f594adbc8488b3",
+    "url": "./static/media/hire.07d5212d.jpg"
+  },
+  {
+    "revision": "de4c646964ae4dfb4f69a1b077b15da5",
+    "url": "./static/media/mission.de4c6469.svg"
+  },
+  {
+    "revision": "88556646f36a87deb50fa62baaa19bf6",
+    "url": "./static/media/mobile_development.88556646.svg"
+  },
+  {
+    "revision": "b3ba856629d8f44944c53508eaddb7fd",
+    "url": "./static/media/office.b3ba8566.png"
+  },
+  {
+    "revision": "95b2483e22dc1b359e044b3f1d35d948",
+    "url": "./static/media/problems.95b2483e.svg"
+  },
+  {
+    "revision": "c3a94ddcb0486278b49f02eff252acc8",
+    "url": "./static/media/team.c3a94ddc.png"
+  },
+  {
+    "revision": "068b1c3456f23d56ea00acdb6893004b",
+    "url": "./static/media/top.068b1c34.svg"
+  },
+  {
+    "revision": "831f3735a4a0ffb801639498f87f34f9",
+    "url": "./static/media/ui.831f3735.svg"
+  },
+  {
+    "revision": "8cbe89e1a1475d989133fa7195c379ae",
+    "url": "./static/media/ux.8cbe89e1.jpg"
+  }
+]);
